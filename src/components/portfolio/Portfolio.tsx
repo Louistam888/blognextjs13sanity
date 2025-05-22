f323ac2fed843c6d54948d7b117ab2d222df5bc6
Louistam888 <louistam888@gmail.com>
2025-05-22 19:08:21 -0400

    installed next-sanity

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./page.module.css";
+
+//grok query
+async function getData() {}
 
 const Portfolio = () => {
-  return <div>P</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Portfolio</h1>
+      <div className={styles.items}></div>
+    </div>
+  );
 };
 
 export default Portfolio;
