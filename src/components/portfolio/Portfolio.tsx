21965d1f06571701092f7089b97b2e2168982f1f
Louistam888 <louistam888@gmail.com>
2025-05-26 20:50:11 -0400

    grok fetch successful

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import styles from "./page.module.css";
+import { client } from "../../utils/configSanity";
+import { get } from "http";
 
-//grok query
-async function getData() {}
+interface IPortfolio {
+  _id: string;
+  title: string;
+  description: any; //textbox
+  _createdAt: string;
+}
 
-const Portfolio = () => {
+async function getData() {
+  const query = `*[_type == "portfolio"]`;
+  const data = await client.fetch(query);
+
+  return data as IPortfolio[];
+}
+
+const Portfolio = async () => {
+  const data = (await getData()) as IPortfolio[];
+
   return (
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Portfolio</h1>
--- src/utils/configSanity.ts
+++ src/utils/configSanity.ts
@@ -1,9 +1,8 @@
-import { createClient } from 'next-sanity';
+import { createClient } from "next-sanity";
 
-export const client = createClient ({
-    projectId: '',
-    dataset: '',
-    apiVersion: '',
-})
-
-// 1650
+export const client = createClient({
+  projectId: "2wt3vq16",
+  dataset: "production",
+  apiVersion: "2025-05-27",
+  useCdn: true,
+});
